03a2069bc8b2184e894a632656fc0f1639170f25
flaneuse <laura.d.hughes@gmail.com>
2016-02-10 23:27:50 -0500

    themes

--- R/loadPkgs.R
+++ R/loadPkgs.R
@@ -4,26 +4,29 @@
 #' @export
 
 
-loadPkgs <- function(pkgGrp = 'all') {
+loadPkgs <- function(pkgGrp = 'all', quiet = FALSE) {
   
   # Package list ------------------------------------------------------------
   
-!! Look for other packages
-  !! Fix fonts
   
   # -- Data manipulation -- 
-  wrangling = c('dplyr', 'tidyr', 'data.table', 'stringr', 'lubridate')
+  wrangling = c('dplyr', 'tidyr', 'data.table', 'stringr', 
+                'lubridate', 'purrr', 'zoo')
   # dplyr: filter, create new variables, summarise, ... Basically, anything you can think to do to a dataset
   # tidyr: Reshape and merge datasets
   # data.table: similar to dplyr but good for large datasets; some extra functionality
   # stringr: String manipulation
   # lubridate: dates
+  # zoo: running averages, amongst other things.
   
   
   # -- Plotting / viz functions -- 
   plotting = c('ggplot2', 'ggvis', 'htmltools', 'htmlwidgets',
                'metricsgraphics', 'rCharts', 'plotly', 'ggmap',
-               'choroplethrAdmin1', 'choroplethr')  
+               'choroplethrAdmin1', 'choroplethr', 'd3heatmap',
+               'DiagrammeR', 'ggrepel', 'hexbin', 'lattice', 
+               'latticeExtra', 'packcircles', 'rbokeh', 'waffle',
+               'RgoogleMaps', 'shiny', 'shinydashboard', 'shinythemes')  
   # ggplot2: Incredibly powerful plotting library built off of the "Grammer of Graphics"
   # ggmpap: geocoding and geospatial library
   
@@ -33,7 +36,7 @@ loadPkgs <- function(pkgGrp = 'all') {
   
   
   # -- Libraries to help import files --
-  importing = c('haven', 'readr', 'readxl', 'googlesheets', 'rvest')
+  importing = c('haven', 'readr', 'readxl', 'googlesheets', 'rvest', 'foreign')
   # haven: Imports in files from Stata, SAS, and SPSS
   # readr: An advanced form of the base 'read.csv' file with some added functionality.
   # readxl: Functions to import in multiple sheets from Excel
@@ -41,10 +44,12 @@ loadPkgs <- function(pkgGrp = 'all') {
   # rvest: Scrapes websites
   
   # -- Developer libraries --
-  devPkgs = c('roxygen2', 'testthat')
+  devPkgs = c('roxygen2', 'testthat', 'jsonlite', 'microbenchmark',
+              'profvis')
   
   # -- Fitting libraries --
-  fitting = c('MASS', 'sandwich', 'lmtest', 'plm', 'ggalt', 'coefplot')
+  fitting = c('MASS', 'sandwich', 'lmtest', 'plm', 'ggalt', 'coefplot', 'broom', 
+              'cluster', 'GWmodel')
 
   
 
@@ -66,12 +71,14 @@ loadPkgs <- function(pkgGrp = 'all') {
   
   # Install anything that isn't already installed.
   if(length(toInstall > 0)) { 
-    install.packages(new.packages)
+    print(paste0('Installing these packages: ', toInstall))
+    
+    install.packages(toInstall)
   }
   
   # Load packages
-  for (i in 1:length(pkgs)) {
-    library(pkgs[i], character.only = TRUE)
+  for (i in seq_along(pkgs)) {
+    library(pkgs[i], character.only = TRUE, quietly = quiet)
   }
   
   loadfonts(quiet = TRUE)
--- R/themes.R
+++ R/themes.R
@@ -1,14 +1,17 @@
-#' Theme with x and y axis labels
-#' 
-#' This function can be tacked onto a ggplot object to specify a new theme with the x and y axis labeled.
+#' Customized ggplot themes
+#'
+#' @param none
+#' @name themes
+NULL
+#> NULL
 #' @export
 #' @examples
-#' ggplot(mtcars, aes(x = mpg, y = cyl)) + geom_point() + theme_xylab()
+#' ggplot(mtcars, aes(x = mpg, y = wt, colour = cyl)) + geom_point() + theme_xylab()
+#' 
 
+
+#' @describeIn themes Theme with x and y axis labels
 theme_xylab<- function() {
-  grey60K = 'grey'
-  grey90K = 'grey'
-  
   theme_bw() +
     theme(
       text = element_text(family = 'Segoe UI Light', colour = grey60K),
@@ -26,4 +29,191 @@ theme_xylab<- function() {
       panel.grid.minor.y = element_blank(),
       panel.grid.minor.x = element_blank(),
       panel.grid.major.x = element_blank())
+}
+
+
+#' @describeIn  themes Theme with x labels, x title, and x-axis line
+#' @export
+theme_xaxis <- function() {
+  theme_bw() +
+    theme(
+      text = element_text(family = 'Segoe UI Light', colour = grey60K),
+      rect = element_blank(),
+      plot.background = element_blank(),
+      axis.text = element_text(size = 12,  color = grey60K),
+      axis.text.y = element_blank(),
+      title =  element_text(size = 15, family = "Segoe UI", hjust = 0, color = grey90K),
+      axis.title =  element_text(size = 14, family = "Segoe UI Semilight", color = grey60K, hjust = 0.5, vjust = -0.25),
+      axis.title.y = element_blank(),
+      strip.text = element_text(size=14, face = 'bold', hjust = 0.05, vjust = -2.5, color = '#4D525A'),
+      legend.position = 'none',
+      strip.background = element_blank(),
+      axis.ticks = element_blank(),
+      panel.margin = unit(3, 'lines'),
+      axis.line = element_line(size = 0.2, colour = grey50K),
+      axis.line.y = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_blank()
+    )
+}
+
+#' @describeIn  themes Theme with y labels, titles, and y-axis line
+theme_yaxis <- function() {
+  theme_bw() +
+    theme(
+      text = element_text(family = 'Segoe UI Light', colour = grey60K),
+      rect = element_blank(),
+      plot.background = element_blank(),
+      axis.text = element_text(size = 12,  color = grey60K),
+      axis.text.x = element_blank(),
+      title =  element_text(size = 15, family = "Segoe UI", hjust = 0, color = grey90K),
+      axis.title =  element_text(size = 14, family = "Segoe UI Semilight", color = grey60K, hjust = 0.5, vjust = -0.25),
+      axis.title.x = element_blank(),
+      strip.text = element_text(size=14, face = 'bold', hjust = 0.05, vjust = -2.5, color = '#4D525A'),
+      legend.position = 'none',
+      strip.background = element_blank(),
+      axis.ticks = element_blank(),
+      panel.margin = unit(3, 'lines'),
+      axis.line = element_line(size = 0.2, colour = grey50K),
+      axis.line.x = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_blank()
+    )
+}
+
+
+#' @describeIn  themes Theme with x and y labels, titles, and gridlines
+theme_xygrid<- function() {
+  theme_bw() +
+    theme(
+      text = element_text(family = 'Segoe UI Light', colour = grey60K),
+      rect = element_blank(),
+      plot.background = element_blank(),
+      axis.text = element_text(size = 12,  color = grey60K),
+      title =  element_text(size = 15, family = "Segoe UI", hjust = 0, color = grey90K),
+      axis.title =  element_text(size = 14, family = "Segoe UI Semilight", color = grey60K, hjust = 0.5, vjust = -0.25),
+      strip.text = element_text(size=14, face = 'bold', hjust = 0.05, vjust = -2.5, color = '#4D525A'),
+      legend.position = 'none',
+      strip.background = element_blank(),
+      axis.ticks = element_blank(),
+      panel.margin = unit(3, 'lines'),
+      panel.grid.major.y = element_line(size = 0.2, color = grey30K),
+      panel.grid.minor.y = element_blank(),
+      panel.grid.minor.x = element_blank(),
+      panel.grid.major.x = element_line(size = 0.1, color = grey30K))
+}
+
+#' @describeIn  themes Theme with axis labels, titles, grid lines, axes, and legend.
+theme_basic <- function() {
+  theme_bw() +
+    theme(
+      text = element_text(family = 'Segoe UI Light', colour = grey60K),
+      plot.background = element_blank(),
+      panel.border = element_rect(size = 0.2, colour = grey90K, fill = NA),
+      axis.text = element_text(size = 12,  color = grey60K),
+      title =  element_text(size = 15, family = "Segoe UI", hjust = 0, color = grey90K),
+      axis.title =  element_text(size = 14, family = "Segoe UI Semilight", color = grey60K, hjust = 0.5, vjust = -0.25),
+      strip.background = element_blank(),
+      axis.ticks = element_blank(),
+      panel.margin = unit(3, 'lines'),
+      panel.grid.major.y = element_line(size = 0.2, color = grey30K),
+      panel.grid.minor.y = element_blank(),
+      panel.grid.minor.x = element_blank(),
+      panel.grid.major.x = element_line(size = 0.1, color = grey30K),
+      strip.text = element_text(size=13, color = grey50K, family = 'Segoe UI Semilight'),
+      legend.position = c(0.85, 0.85),
+      legend.title = element_text(size = 13),
+      legend.text = element_text(size = 11),
+      legend.background = element_blank(),
+      strip.background = element_blank()
+    )
+}
+
+
+#' @describeIn  themes completely blank theme; similar to theme_void but without legend or margins.
+theme_blank <- function() {
+  theme(
+    title = element_blank(),
+    axis.title = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    axis.ticks.length = unit(0, units = 'points'),
+    axis.ticks.margin = unit(0, units =  'points'),
+    panel.border = element_blank(),
+    panel.grid = element_blank(),
+    panel.background = element_blank(), 
+    plot.background = element_blank(), 
+    legend.position="none"
+  )
+}
+
+#' @describeIn  themes Theme with only x and y labels; no axes, axis titles, or legends
+theme_labelsOnly <- function() {
+  theme(
+    title =  element_text(size = 16, hjust = 0, color = grey90K,
+                          family = 'Segoe UI'),
+    axis.title = element_blank(),
+    axis.text = element_text(size = 12, hjust = 0.5, 
+                             color = grey60K, family = 'Segoe UI Light'),
+    axis.ticks = element_blank(),
+    panel.border = element_blank(),
+    plot.margin = rep(unit(0, units = 'points'),4),
+    panel.grid = element_blank(),
+    panel.background = element_blank(), 
+    plot.background = element_blank(), 
+    legend.position="none"
+  )
+}
+
+#' @describeIn  themes Theme with light x-grid lines, x and y axis labels, and x-axis title.
+theme_xgrid<- function() {
+  theme(title = element_text(size = 16, color = grey90K, 
+                             family = 'Segoe UI'),
+        axis.line = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text.x = element_text(size = 16, color = grey60K, family = 'Segoe UI Light'),
+        axis.title.x = element_text(size = 18, color = grey60K, family = 'Segoe UI Semilight'),
+        axis.text.y = element_text(size = 16, color = grey60K, family = 'Segoe UI Light'),
+        axis.title.y = element_blank(), 
+        axis.line.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.position="none",
+        panel.background = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major.x = element_line(size = 0.1, colour = grey60K),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.border = element_blank(),
+        plot.margin = rep(unit(0, units = 'points'),4),
+        panel.background = element_blank(), 
+        strip.text = element_text(size=13, face = 'bold'),
+        strip.background = element_blank()
+  )
+}
+
+
+#' @describeIn  themes Theme with light y-grid lines, x and y axis labels, and y-axis title.
+theme_ygrid<- function() {
+  theme(title = element_text(size = 16, color = grey90K, 
+                             family = 'Segoe UI'),
+        axis.line = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text.x = element_text(size = 16, color = grey60K, family = 'Segoe UI Light'),
+        axis.title.y = element_text(size = 18, color = grey60K, family = 'Segoe UI Semilight'),
+        axis.text.y = element_text(size = 16, color = grey60K, family = 'Segoe UI Light'),
+        axis.title.x = element_blank(), 
+        axis.line.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.position="none",
+        panel.background = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_line(size = 0.1, colour = grey60K),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.border = element_blank(),
+        plot.margin = rep(unit(0, units = 'points'),4),
+        panel.background = element_blank(), 
+        strip.text = element_text(size=13, face = 'bold'),
+        strip.background = element_blank()
+  )
 }
