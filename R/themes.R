35dcca424e5ccd0d7e71f4f11a9bd9de1f245fae
flaneuse <laura.d.hughes@gmail.com>
2016-02-11 09:34:16 -0500

    images

--- R/themes.R
+++ R/themes.R
@@ -126,7 +126,6 @@ theme_blank <- function() {
     axis.text = element_blank(),
     axis.ticks = element_blank(),
     axis.ticks.length = unit(0, units = 'points'),
-    axis.ticks.margin = unit(0, units =  'points'),
     panel.border = element_blank(),
     panel.grid = element_blank(),
     panel.background = element_blank(), 
