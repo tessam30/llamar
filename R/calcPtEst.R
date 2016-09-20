8082886df81d4f71bf8152f7fbeba7978743f9e8
Laura Hughes <laura.d.hughes@gmail.com>
2016-09-20 15:24:14 -0400

    add svymean

--- R/calcPtEst.R
+++ R/calcPtEst.R
@@ -5,28 +5,56 @@
 #'
 
 #' @author Laura Hughes, laura.d.hughes@gmail.com
+#' 
+#' @param df main data frame containing the raw data
+#' @param var string containing the variable name you want to average
+#' @param by_var (optional) string containing the variable name over which you want to calculate the estimates
 #' @param 
-#' @examples
-#' 
-
-#' 
-
 #' @export
+#' 
+
+
 # -- Wrapper to survey::svyby --
 # Calculates point estimates with standard errors, weighted by sample weights
-calcPtEst = function(var, # What you want to average
-                     by_var, # variable 
-                     design, # survey design object (containing sampling procedure)
-                     df, # main data frame containing raw data
+calcPtEst = function(df, # main data frame containing raw data,
+                     var, # What you want to average
+                     by_var = NA, # variable 
+                     design = NA, # survey design object (containing sampling procedure)
+                     psu_var = NA,
+                     strata_var = NA,
+                     weight_var = NA,
                      omit_NA = TRUE,
                      ci_factor = 2) {
   
-  # Calculate point estimate and standard error.
-  pt_est = svyby(as.formula(paste0('~', var)), 
-                 by = as.formula(paste0('~', by_var)),
-                 design = design,
-                 svymean,
-                 na.rm = omit_NA)
+  # -- define survey design -- 
+  # create the survey design object (containing sampling procedure) if not specified
+  if (is.na(design)){
+    # Check all the correct variables are inputted
+    if(is.na(psu_var) | is.na(strata_var) | is.na(weight_var)) {
+      stop('Survey design not specified')
+    }
+    
+    design = svydesign(id = as.formula(paste0('~', psu_var)), 
+                       strata = as.formula(paste0('~', strata_var)), 
+                       weights = as.formula(paste0('~', weight_var)), 
+                       data = df)
+  }
+  
+  if(is.na(by_var)) {
+    # -- just calculate the survey mean --
+    pt_est = svymean(as.formula(paste0('~', var)), 
+                     by = as.formula(paste0('~', by_var)),
+                     design = design,
+                     na.rm = omit_NA)
+  } else {
+    # Calculate point estimate and standard error.
+    pt_est = svyby(as.formula(paste0('~', var)), 
+                   by = as.formula(paste0('~', by_var)),
+                   design = design,
+                   svymean,
+                   na.rm = omit_NA)
+  }
+  
   # Convert to data frame, if not already
   pt_est = as.data.frame(pt_est)
   
