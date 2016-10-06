cefeed78e6d4c69f960c84967a3c17f6cbe55bae
flaneuse <laura.d.hughes@gmail.com>
2016-10-06 11:59:13 -0400

    case when ggplot args

--- R/plot_relationships.R
+++ R/plot_relationships.R
@@ -1,24 +1,25 @@
+#' Plot relationships between variables in a model 
+#' 
+#' Loops through independent variables within a model and 
+#' plots their relationship with the dependent variable
+#' 
+#' @import ggplot2 dplyr forcats RColorBrewer
+#' 
+#' @param model Fitted model
 
-
-ggplot(ch, aes(x = rural_cat, y = stuntingZ)) +
-  geom_boxplot() +
-  annotate(geom = 'text', label = 'n') +
-  scale_y_continuous(limits = stunting_range)
-
-ggplot(ch, aes(x = rural_cat, y = isStunted)) +
-  stat_summary(geom = 'point', size = 5, fun.y = 'mean') +
-  scale_y_continuous(limits = stunted_range)
-
-ggplot(ch, aes(x = wealth_idx, y = isStunted)) +
-  geom_smooth() +
-  theme_bw() +
-  scale_y_continuous(limits = stunted_range)
-
+#' @examples
+#' data(diamonds)
+#' model1 <- lm(price ~ carat + cut*color, data=diamonds)
+#' plot_relationships(model1)
+#' 
+#' @export
 
 plot_relationships = function(model, 
                               binary_y = FALSE,
-                              stunting_range = c(-4, 4),
-                              stunted_range = c(0, 1)){
+                              cont_range = c(-4, 4),
+                              binary_range = c(0, 1),
+                              binary_colour = '#66c2a5',
+                              cont_colour = '#3288bd'){
   
   # Pull out the data from the model
   df = model$model
@@ -43,16 +44,42 @@ plot_relationships = function(model,
     if(binary_y == TRUE & indpndt_vars$type[i] == 'factor') {
       
       p = ggplot(df, aes_string(x = indpndt_vars$variable[i], y = dpndt_var$variable)) +
-        stat_summary(geom = 'point', size = 5, fun.y = 'mean_cl_boot', colour = '#66c2a5') +
-        scale_y_continuous(limits = stunted_range) +
+        stat_summary(geom = 'point', size = 5, fun.y = 'mean_cl_boot', colour = binary_colour) +
+        scale_y_continuous(limits = binary_range, labels = scales::percent) +
         theme_ygrid()
       
       print(p)
       readline()
+      
     } else if (binary_y == TRUE & indpndt_vars$type[i] == 'numeric') {
-        
-      } 
-
+      p = ggplot(df, aes_string(x = indpndt_vars$variable[i], y = dpndt_var$variable)) +
+        geom_smooth(colour = binary_colour) +
+        scale_y_continuous(limits = binary_range, labels = scales::percent) +
+        theme_xygridlight()
+      
+      print(p)
+      readline()
+      
+    } else if(indpndt_vars$type[i] == 'factor') {
+      p = ggplot(df, aes_string(x = indpndt_vars$variable[i], 
+                                y = dpndt_var$variable,
+                                fill = indpndt_vars$variable[i])) +
+        geom_boxplot(alpha = 0.5) +
+        scale_y_continuous(limits = cont_range) +
+        scale_fill_brewer(palette = 'Spectral') +
+        theme_ygrid()
+      
+      print(p)
+      readline()
+      
+    } else if(indpndt_vars$type[i] == 'numeric')
+      p = ggplot(df, aes_string(x = indpndt_vars$variable[i], y = dpndt_var$variable)) +
+        geom_smooth(colour = cont_colour) +
+        scale_y_continuous(limits = cont_range) +
+        theme_xygridlight()
+    
+    print(p)
+    readline()
     
   }
   
