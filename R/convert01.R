3cbd6947bf96bdd66cda4bc9720d96973a03d4fb
flaneuse <laura.d.hughes@gmail.com>
2016-12-28 14:59:31 -0500

    generalize beyond 1/2

--- R/convert01.R
+++ R/convert01.R
@@ -5,6 +5,8 @@
 #' @param df to mutate.
 #' @param varName variable to convert 1's and 2's to 1's and 0's
 #' @param newVar variable name of new variable
+#' @param trueVal value(s) to be coded as 1
+#' @param falseVal value(s) to be coded as 0
 #' @export
 #' 
 #' @examples
@@ -12,11 +14,19 @@
 #' 
 #' df %>% convert01(varName = 'x', newVar = 'x2')
 #' 
-convert01 = function(df, varName, newVar) {
+#' # Change what's TRUE and FALSE
+#' df %>% convert01(varName = 'x', newVar = 'x2', trueVal = 9, falseVal = '0:2')
+#' df %>% convert01(varName = 'x', newVar = 'x2', trueVal = 'c(2,9)', falseVal = '1')
+#' 
+convert01 = function(df, 
+                     varName, 
+                     newVar,
+                     trueVal = '1', 
+                     falseVal = '2') {
   # Check if is 2, 1, or another value.
   df2 = df %>% 
-    mutate_(.dots = setNames(paste0("ifelse(", varName, " == 2, 0, 
-         ifelse(", varName, "== 1, 1, NA))"), newVar))
+    mutate_(.dots = setNames(paste0("ifelse(", varName, " %in%", falseVal, ", 0, 
+         ifelse(", varName, "%in%", trueVal, ", 1, NA))"), newVar))
   
   return(df2)
 }
