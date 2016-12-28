577b9af4d3669f6a128ff5807f27fddca62911fe
flaneuse <laura.d.hughes@gmail.com>
2016-12-28 14:33:21 -0500

    documentation

--- R/convert01.R
+++ R/convert01.R
@@ -2,11 +2,21 @@
 #' 
 #' Function to convert survey data where 2 is no and 1 is yes.
 #' 
+#' @param df to mutate.
 #' @param varName variable to convert 1's and 2's to 1's and 0's
+#' @param newVar variable name of new variable
 #' @export
 #' 
-convert01 = function(varName) {
-    # Check if
-    paste0("ifelse(", varName, " == 2, 0, 
-         ifelse(", varName, "== 1, 1, NA))")
+#' @examples
+#' df = data.frame(x = c(2, 1, 9, NA), y = sample(100, 4))
+#' 
+#' df %>% convert01(varName = 'x', newVar = 'x2')
+#' 
+convert01 = function(df, varName, newVar) {
+  # Check if is 2, 1, or another value.
+  df2 = df %>% 
+    mutate_(.dots = setNames(paste0("ifelse(", varName, " == 2, 0, 
+         ifelse(", varName, "== 1, 1, NA))"), newVar))
+  
+  return(df2)
 }
