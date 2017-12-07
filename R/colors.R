78d8682f766679e12cdb9ffe11d7f2720d61fcba
tessam30 <tessam30@users.noreply.github.com>
2017-12-07 11:59:20 -0500

    Update colors.R

--- R/colors.R
+++ R/colors.R
@@ -154,6 +154,48 @@ define_colors = function(save_colors = FALSE){
   ftfGreen = "#94A545"
   ftfOrange = "#D37D28"
   
+  # Add in new colors from https://carto.com/carto-colors/
+
+# Continous schemes from carto
+lipstick1 = c("#ffc6c4", "#f4a3a8", "#e38191", "#cc607d", "#ad466c", "#8b3058", "#672044")
+lipstick2 = c("#fbe6c5", "#f5ba98", "#ee8a82", "#dc7176", "#c8586c", "#9c3f5d", "#70284a")
+lipstick3 = c("#f6d2a9", "#f5b78e", "#f19c7c", "#ea8171", "#dd686c", "#ca5268", "#b13f64")
+reds1_carto = c("#ecda9a", "#efc47e", "#f3ad6a", "#f7945d", "#f97b57", "#f66356", "#ee4d5a")
+reds2_carto =c("#fde0c5", "#facba6", "#f8b58b", "#f59e72", "#f2855d", "#ef6a4c", "#eb4a40")
+red33_carto = c("#fef6b5", "#ffdd9a", "#ffc285", "#ffa679", "#fa8a76", "#f16d7a", "#e15383")
+
+seagreen = c("#e4f1e1", "#b4d9cc", "#89c0b6", "#63a6a0", "#448c8a", "#287274", "#0d585f")
+seagreen1 = c("#c4e6c3", "#96d2a4", "#6dbc90", "#4da284", "#36877a", "#266b6e", "#1d4f60")
+seagreen2 = c("#d2fbd4", "#a5dbc2", "#7bbcb0", "#559c9e", "#3a7c89", "#235d72", "#123f5a")
+seagreen3 = c("#d3f2a3", "#97e196", "#6cc08b", "#4c9b82", "#217a79", "#105965", "#074050")
+seagreen4 = c("#f7feae", "#b7e6a5", "#7ccba2", "#46aea0", "#089099", "#00718b", "#045275")
+seagreen5 = c("#d1eeea", "#a8dbd9", "#85c4c9", "#68abb8", "#4f90a6", "#3b738f", "#2a5674")
+seagreen6 = c("#b0f2bc", "#89e8ac", "#67dba5", "#4cc8a3", "#38b2a3", "#2c98a0", "#257d98")
+
+purp1_carto = c("#f3e0f7", "#e4c7f1", "#d1afe8", "#b998dd", "#9f82ce", "#826dba", "#63589f")
+purp2_carto = c("#f9ddda", "#f2b9c4", "#e597b9", "#ce78b3", "#ad5fad", "#834ba0", "#573b88")
+purp3_carto = c("#f3e79b", "#fac484", "#f8a07e", "#eb7f86", "#ce6693", "#a059a0", "#5c53a5")
+purp4_carto = c("#f3cbd3", "#eaa9bd", "#dd88ac", "#ca699d", "#b14d8e", "#91357d", "#6c2167")
+purp5_carto = c("#fcde9c", "#faa476", "#f0746e", "#e34f6f", "#dc3977", "#b9257a", "#7c1d6f")
+purp6_carto = c("#ede5cf", "#e0c2a2", "#d39c83", "#c1766f", "#a65461", "#813753", "#541f3f")
+
+# Divergent schemes
+grn_pnk = c("#798234", "#a3ad62", "#d0d3a2", "#fdfbe4", "#f0c6c3", "#df91a3", "#d46780")
+grn_org = c("#3d5941", "#778868", "#b5b991", "#f6edbd", "#edbb8a", "#de8a5a", "#ca562c")
+trq_org = c("#008080", "#70a494", "#b4c8a8", "#f6edbd", "#edbb8a", "#de8a5a", "#ca562c")
+trq_pnk = c("#009392", "#39b185", "#9ccb86", "#e9e29c", "#eeb479", "#e88471", "#cf597e")
+trq_pnk2 = c("#009392", "#72aaa1", "#b1c7b3", "#f1eac8", "#e5b9ad", "#d98994", "#d0587e")
+trq_prp = c("#009B9E", "#42B7B9", "#A7D3D4", "#F1F1F1", "#E4C1D9", "#D691C1", "#C75DAB")
+brn_blu = c("#A16928", "#bd925a", "#d6bd8d", "#edeac2", "#b5c8b8", "#79a7ac", "#2887a1")
+
+# Qualitative schemes
+qual1 = c("#855C75", "#D9AF6B", "#AF6458", "#736F4C", "#526A83", "#625377", "#68855C", "#9C9C5E", "#A06177", "#8C785D", "#467378", "#7C7C7C")
+qual2 = c("#7F3C8D", "#11A579", "#3969AC", "#F2B701", "#E73F74", "#80BA5A", "#E68310", "#008695", "#CF1C90", "#f97b72", "#4b4b8f", "#A5AA99")
+qual3 = c("#66C5CC", "#F6CF71", "#F89C74", "#DCB0F2", "#87C55F", "#9EB9F3", "#FE88B1", "#C9DB74", "#8BE0A4", "#B497E7", "#D3B484", "#B3B3B3")
+qual4 = c("#5F4690", "#1D6996", "#38A6A5", "#0F8554", "#73AF48", "#EDAD08", "#E17C05", "#CC503E", "#94346E", "#6F4070", "#994E95", "#666666")
+qual5 = c("#88CCEE", "#CC6677", "#DDCC77", "#117733", "#332288", "#AA4499", "#44AA99", "#999933", "#882255", "#661100", "#6699CC", "#888888")
+qual6 = c("#E58606", "#5D69B1", "#52BCA3", "#99C945", "#CC61B0", "#24796C", "#DAA51B", "#2F8AC4", "#764E9F", "#ED645A", "#CC3A8E", "#A5AA99")
+  
   
   if(save_colors == TRUE){
     
@@ -207,7 +249,39 @@ define_colors = function(save_colors = FALSE){
                'USAID_dkred',
                'ftfBlue',
                'ftfGreen',
-               'ftfOrange')
+               'ftfOrange',
+               'brn_blu', 
+               'grn_org',
+               'grn_pnk', 
+               'lipstick1', 
+               'lipstick2', 
+               'lipstick3', 
+               'purp1_carto', 
+               'purp2_carto', 
+               'purp3_carto', 
+               'purp4_carto', 
+               'purp5_carto', 
+               'purp6_carto', 
+               'qual1', 
+               'qual2', 
+               'qual3', 
+               'qual4', 
+               'qual5', 
+               'qual6', 
+               'red33_carto', 
+               'reds1_carto', 
+               'reds2_carto', 
+               'seagreen', 
+               'seagreen1', 
+               'seagreen2', 
+               'seagreen3', 
+               'seagreen4', 
+               'seagreen5', 
+               'seagreen6', 
+               'trq_org', 
+               'trq_pnk', 
+               'trq_pnk2', 
+               'trq_prp')
     
     lapply(colors, function(x) save(list = x, file = paste0('data/', x, '.rda')))
   }
